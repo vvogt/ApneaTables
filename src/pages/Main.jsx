175bf2cab883a3ba31e9c12c9a72bb72f190abed
Madis Otenurm <Madis0@users.noreply.github.com>
2020-05-13 15:42:26 +0300

    First attempt

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,11 +3,13 @@ import { View } from "react-native";
 import { styles } from "../styles/_main.js";
 import { BottomTabs } from "../components/BottomTabs";
 import Oxygen from "../components/Oxygen";
+import Circle from "../components/Circle";
 
 export default function Main() {
   return (
     <View style={styles.mainContainer}>
       <Oxygen />
+      <Circle />
       <BottomTabs />
     </View>
   );
