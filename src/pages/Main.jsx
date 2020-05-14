296f3222a17d484d694991aec7552789b8c61847
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2020-05-14 16:23:03 +0300

    Created the oxygen table

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,11 +3,13 @@ import { View } from "react-native";
 import { styles } from "../styles/_main.js";
 import { BottomTabs } from "../components/BottomTabs";
 import Carbon from "../components/Carbon";
+import Oxygen from "../components/Oxygen";
 
 export default function Main() {
   return (
     <View style={styles.mainContainer}>
-      <Carbon />
+      {/* <Carbon /> */}
+      <Oxygen />
       <BottomTabs />
     </View>
   );
